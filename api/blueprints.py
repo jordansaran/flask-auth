12ecf320ae07e1d9b3781ed7053a7ac83c6ae2ae
Jordan Ferreira Saran <jfsaran@gmail.com>
2024-09-19 07:14:33 -0300

    feat: ✨ set authorization to swagger

--- api/blueprints.py
+++ api/blueprints.py
@@ -7,6 +7,14 @@ from api.src.namespaces.auth import ns_token
 from api.src.namespaces.user import ns_user
 from core.settings import Settings
 
+authorizations = {
+    'Bearer Auth': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization',
+    'description': 'Coloque o token JWT aqui: Bearer {token}'
+    }
+}
 
 api_bp_pipeline = Blueprint(name='api', import_name=__name__, url_prefix='/api/v1')
 settings = Settings()
@@ -14,6 +22,8 @@ api_pipeline = Api(
     api_bp_pipeline,
     version='1.0.0',
     title='Flask Auth - API',
+    security='Bearer Auth',
+    authorizations=authorizations,
     description='Bem vindo a UI documentacão Flask Auth com Swagger',
     doc='/ui' if Settings().debug_is_enabled() else False,
     validate=True
